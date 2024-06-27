daf75773017a06875ff8ebb20f46132ee26778ec
Prafull Gaikwad <prgaikw@microsoft.com>
2024-06-27 00:57:49 -0700

    Add auth controller, sign up functionality and errorhandler middleware

--- api/index.js
+++ api/index.js
@@ -2,28 +2,47 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from '../routes/user.route.js'
+import authRouter from '../routes/auth.route.js'
 
 dotenv.config() 
 
 const app = express()
 
 // connect to mongodb
-
 mongoose.connect(process.env.MONGO_CONNECTION_STRING).then(() => {
     console.log("Succcessfully connected to MongoDB");
 }).catch((err) => {
     console.log("Could not connect to MONGO!! " + err) 
 })
 
+
+// middleware
+app.use(express.json())
+
+app.listen(3001, () => {
+    console.log("Server started listening at 3001")
+})
+
+
+app.use('/users', userRoutes)
+app.use('/auth', authRouter)
+const errorHandler = (err,req, res, next) => {
+    console.log("Inside middleware!!")
+    const errorCode = err.statusCode || 500
+    const errorMessage = err.message
+
+    return res.status(errorCode).json({
+        message: errorMessage,
+        errorCode,
+        success: false
+    })
+}
+
+app.use(errorHandler)
+
 app.get('/', (req, res) => {
     console.log("Welcome home")
     res.status(200).send({
         "message": "Welcome Home"
     })
-})
-
-app.use('/users', userRoutes)
-
-app.listen(3001, () => {
-    console.log("Server started listening at 3001")
 })
